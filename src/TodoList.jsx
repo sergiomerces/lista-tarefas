4f76d7f90123781d30d5bcd06d800707cc6f30cb
sergiomerces <sergio.merces@gmail.com>
2023-02-20 20:55:22 -0300

    elementos iniciais do projeto

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 import "./TodoList.css";
 
 function TodoList() {
-    return (<h1>Olá Mundo!</h1>)
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+            <form>
+                <input 
+                    type="text"
+                    placeholder="Adicione uma tarefa"
+                />
+                <button type="submit">Add</button>
+            </form>
+            <div className="lista-tarefas">
+                <div className="item">
+                    <span>Tarefa de Exemplo</span>
+                    <button>Deletar</button>
+                </div>
+                <div className="item-completo">
+                    <span>Tarefa de Exemplo</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default TodoList;
