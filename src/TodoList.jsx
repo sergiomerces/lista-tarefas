ae4a7120ba7d1bf848fdeae2bd0c6b1d20eb330c
sergiomerces <sergio.merces@gmail.com>
2023-02-21 02:44:30 -0300

    tornando a lista dinâmica

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TodoList.css";
+import Icone from "./assets/checklist.svg"
 
 function TodoList() {
+
+    const [lista, setLista] = useState([]);
+    const [novoItem, setNovoItem] = useState("");
+
+    function adicionaItem(form) {
+        form.preventDefault();
+        
+        if(!novoItem) {
+            return;
+        }
+
+        setLista([...lista, { text: novoItem, isCompleted: false }])
+        setNovoItem("");
+        document.getElementById('input-entrada').focus();
+    }
+
     return (
         <div>
             <h1>Lista de Tarefas</h1>
-            <form>
+            <form onSubmit={adicionaItem}>
                 <input 
+                    id="input-entrada"
                     type="text"
+                    value={novoItem}
+                    onChange={(e) => { setNovoItem(e.target.value) }}
                     placeholder="Adicione uma tarefa"
                 />
                 <button className="add" type="submit">Add</button>
             </form>
             <div className="lista-tarefas">
-                <div className="item">
-                    <span>Tarefa de Exemplo</span>
-                    <button className="del">Deletar</button>
-                </div>
+                {
+                    lista.length < 1
+                        ?
+                        <img src={Icone} />
+                        :
+                        lista.map((item, index) => (
+                            <div className="item">
+                                <span>Tarefa de Exemplo</span>
+                                <button className="del">Deletar</button>
+                            </div>
+                        ))
+                }
+                
                 <div className="item-completo">
                     <span>Tarefa de Exemplo</span>
                     <button className="del">Deletar</button>
